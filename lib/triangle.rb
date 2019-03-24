7c7da0171315d873c8375c62fd2a7bae710f6536
Cody Yantis <cyantis@gmail.com>
2019-03-24 02:16:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,11 +12,7 @@ class Triangle
     if @side_one <= 0 || @side_two <=0 || @side_three <=0
       raise
     elsif @side_one + @side_two < @side_three || @side_one + @side_three < @side_two || @side_three + @side_two < @side_one
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      raise TriangleError
     elsif @side_one == @side_two && @side_two == @side_three
       :equilateral
     elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
